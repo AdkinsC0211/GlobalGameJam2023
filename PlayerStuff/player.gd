cb51579572967d7e4bed737c5864df941f3f43be
AdkinsC0211 <adkinsc0211@gmail.com>
2023-02-03 23:05:37 -0500

    adedd "grvity"
    youre welcome

--- PlayerStuff/player.gd
+++ PlayerStuff/player.gd
@@ -17,7 +17,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	input = Vector3(Input.get_axis("ui_left", "ui_right"), 0, Input.get_axis("ui_up", "ui_down"))
+	input = Vector3(Input.get_axis("ui_left", "ui_right"), -0.1, Input.get_axis("ui_up", "ui_down"))
 	target_velocity = target_velocity.move_toward(input * speed, delta * 55)
 	velocity = target_velocity.rotated(Vector3.UP, $Camera.rotation.y)
 	move_and_slide(velocity)
